22193eaf72dd1da756e3838aaccfe3d4f6a7c4de
HubertFijolek1 <hubertfijolek1@gmail.com>
2025-01-03 10:48:43 +0100

    Refactor password hashing in crud.py with separate functions

--- backend/app/crud.py
+++ backend/app/crud.py
@@ -10,8 +10,14 @@ def get_user_by_email(db: Session, email: str):
 def get_user_by_username(db: Session, username: str):
     return db.query(models.User).filter(models.User.username == username).first()
 
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
 def create_user(db: Session, user: schemas.UserCreate):
-    hashed_password = pwd_context.hash(user.password)
+    hashed_password = get_password_hash(user.password)
     db_user = models.User(username=user.username, email=user.email, hashed_password=hashed_password)
     db.add(db_user)
     db.commit()
