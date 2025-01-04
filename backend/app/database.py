3eba3288f7dad22c4bd657f7d850cf55b43c9d75
HubertFijolek1 <hubertfijolek1@gmail.com>
2025-01-04 08:56:48 +0100

    Set up SQLAlchemy ORM

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,3 +1,14 @@
-from sqlalchemy.ext.declarative import declarative_base
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
 
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:password@localhost:5432/data_db")
+
+# Create the SQLAlchemy engine
+engine = create_engine(DATABASE_URL, echo=False)
+
+# Create a configured "SessionLocal" class
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Base class for our models
 Base = declarative_base()
