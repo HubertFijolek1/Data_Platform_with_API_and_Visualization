904e63b84b8a3334543776582fd413b04785415f
HubertFijolek1 <hubertfijolek1@gmail.com>
2025-01-14 19:22:18 +0100

    Add relationship between User and File models
    Introduce a foreign key `user_id` in the File model to reference the User model and establish a relationship. This change ensures better data integrity and allows for improved querying of file ownership.

--- backend/api/app/models/models.py
+++ backend/api/app/models/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, DateTime, Integer, String, func
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, String, func
+from sqlalchemy.orm import relationship
 
 from ..database import Base
 
@@ -20,3 +21,5 @@ class Dataset(Base):
     name = Column(String, nullable=False)
     file_name = Column(String, unique=True, nullable=False)
     uploaded_at = Column(DateTime(timezone=True), server_default=func.now())
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    owner = relationship("User")
