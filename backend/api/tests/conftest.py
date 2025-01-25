0432fc650af7568a51b676ea3a45af3c5dc81a53
HubertFijolek1 <hubertfijolek1@gmail.com>
2025-01-25 17:05:28 +0100

    Remove unused test database creation logic.
    Deleted redundant `create_test_db.py` and removed related fixture from `conftest.py`. The test database is no longer needed for the current setup, streamlining the testing process and reducing unnecessary dependencies.

--- backend/api/tests/conftest.py
+++ backend/api/tests/conftest.py
@@ -26,41 +26,6 @@ def load_env():
     load_dotenv(dotenv_path=env_path)
 
 
-@pytest.fixture(scope="session", autouse=True)
-def create_test_database():
-    """
-    Fixture to create the test database before any tests run.
-    """
-    script_path = "/app/scripts/create_test_db.py"
-
-    subprocess.run([sys.executable, script_path], check=True)
-    yield
-    # Teardown: Drop the test database after tests
-    TEST_DATABASE_URL = os.getenv(
-        "TEST_DATABASE_URL",
-        "postgresql://postgres:password@localhost:5432/data_db_test",
-    )
-    parsed = urlparse(TEST_DATABASE_URL)
-    dbname = parsed.path[1:]
-    user = parsed.username
-    password = parsed.password
-    host = parsed.hostname
-    port = parsed.port
-
-    try:
-        conn = psycopg2.connect(
-            dbname="postgres", user=user, password=password, host=host, port=port
-        )
-        conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-        cursor = conn.cursor()
-        cursor.execute(f"DROP DATABASE IF EXISTS {dbname};")
-        print(f"Test database '{dbname}' dropped successfully.")
-        cursor.close()
-        conn.close()
-    except Exception as e:
-        print(f"Error dropping test database: {e}")
-
-
 @pytest.fixture(scope="session")
 def db_engine():
     TEST_DATABASE_URL = os.getenv(
